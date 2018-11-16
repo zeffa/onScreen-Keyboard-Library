4a604ad4a4d63abf644c121e7c2a733cf901cfdf
zeffah <zeffah.elly@gmail.com>
2018-11-16 09:05:59 +0300

    keyboard layout

--- onscreenkeyboard/src/main/java/com/zeffah/onscreenkeyboard/KeyboardView.java
+++ onscreenkeyboard/src/main/java/com/zeffah/onscreenkeyboard/KeyboardView.java
@@ -1,6 +1,26 @@
 package com.zeffah.onscreenkeyboard;
 
+import android.content.Context;
 import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
 
 public class KeyboardView extends ConstraintLayout {
+    public KeyboardView(Context context) {
+        super(context);
+    }
+
+    public KeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
+
+    public KeyboardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context);
+    }
+
+    private void initialize(Context context) {
+        inflate(context, R.layout.keyboard_view, this);
+    }
+
 }
